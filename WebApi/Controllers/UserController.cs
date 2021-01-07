b6d046ee229eccd95612f11ef97729508398aedd
hai.yu <hai.yu@szymr.com>
2021-01-07 18:09:06 +0800

    init

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebApi.Dto;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UserController : ControllerBase
+    {
+      
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("ValidUser")]
+        public Task<OkObjectResult> ValidUser([FromForm] UserDto userDto)
+        {
+            return Task.Run(() => Ok("Received"));
+        }
+    }
+}
---
+++ WebOcelot/Startup.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace WebOcelot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // services.AddHttpReports().AddHttpTransport();
+            services.AddOcelot();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // app.UseHttpReports();
+            app.UseOcelot().Wait();
+        }
+    }
+}
